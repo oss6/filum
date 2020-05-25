0231ea2f7c773454d066d562bedcf6e7acc85a32
oss6 <ossedb@gmail.com>
2020-05-25 14:35:04 +0100

    Fix count_substrings

--- count.ml
+++ count.ml
@@ -4,6 +4,6 @@ let count_substrings s ~substring =
   let rec _inner s acc =
     if String.is_empty s
     then acc
-    else _inner (Chop.first s) (acc + (if String.is_prefix s ~prefix:substring then 1 else 0))
+    else _inner (Chop.last s ~len:(String.length s - 1)) (acc + (if String.is_prefix s ~prefix:substring then 1 else 0))
   in
   _inner s 0
